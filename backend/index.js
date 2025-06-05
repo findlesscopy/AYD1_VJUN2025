f42213ff3136af85279a311ebf8f8096f0278813
José Ibarra <3007542210101@ingenieria.usac.edu.gt>
2025-06-04 18:09:07 -0600

    feat: estructura inicial

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body
+    res.json({
+        message: 'Datos recibidos de manera exitosa',
+        user: username,
+        password: password
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
